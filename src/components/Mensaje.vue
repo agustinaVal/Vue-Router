ee146e2b640a59bec4c9f577f7055918aa16c7c1
Agustina <maria.agustina.valenti89@gmail.com>
2020-09-19 12:58:31 -0300

    primer desafio

---
+++ src/components/Mensaje.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>"Excelente blog"</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +7,49 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contacto',
+    name: 'Contacto',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contacto.vue')
+  },
+  {
+    path: '/post/:entrada',
+    name: 'Post',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Post.vue'),
+    children: [
+      {
+        path: "Mensajes",
+        name: "Mensajes",
+        component: () =>
+          import(
+            /* webpackChunkName: "comentarios" */ "../components/Mensaje.vue"
+          ),
+      },
+    ],
+  },
+  {
+    path: '/',
+    name: 'NotFound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/NotFound.vue')
   }
 ]
 
